bf51fa7cee1dcf6434742ee71ae7edf667a603f5
farukwebcode21 <farukahmadmk@yahoo.com>
2024-10-01 07:08:58 +0600

    module 6 complete

--- src/pages/Home/PopularMenu/PopularMenu.jsx
+++ src/pages/Home/PopularMenu/PopularMenu.jsx
@@ -1,20 +1,26 @@
-import { useState } from "react";
-import SectionTitle from "../../../components/SectionTitle/SectionTitle";
 import { useEffect } from "react";
+import SectionTitle from "../../../components/SectionTitle/SectionTitle";
+import { useState } from "react";
+import Menuitem from "../../Shared/Menuitem";
 
 const PopularMenu = () => {
   const [menu, setMenu] = useState([]);
   useEffect(() => {
-    fetch(menu.json)
-      .then((response) => response.json())
+    fetch("menu.json")
+      .then((res) => res.json())
       .then((data) => {
         const popularItems = data.filter((item) => item.category === "popular");
         setMenu(popularItems);
       });
-  });
+  }, []);
   return (
-    <section>
+    <section className="mb-8">
       <SectionTitle subTile={"Popular Items"} heading={"From Our Menu"} />
+      <div className="grid gap-4 md:grid-cols-2">
+        {menu.map((item) => (
+          <Menuitem key={item._id} item={item} />
+        ))}
+      </div>
     </section>
   );
 };
