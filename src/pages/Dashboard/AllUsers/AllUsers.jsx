768d2a68f87a029b97719a94c4a0eeda3dcc113b
farukwebcode21 <farukahmadmk@yahoo.com>
2024-10-27 17:19:24 +0600

    add all role button

--- src/pages/Dashboard/AllUsers/AllUsers.jsx
+++ src/pages/Dashboard/AllUsers/AllUsers.jsx
@@ -1,14 +1,22 @@
-import { FaRegTrashAlt } from "react-icons/fa";
+import { FaRegTrashAlt, FaUser } from "react-icons/fa";
 import SectionTitle from "../../../components/SectionTitle/SectionTitle";
+import useAxiosSecure from "../../../hooks/useAxiosSecure";
+import { useQuery } from "@tanstack/react-query";
 
 const AllUsers = () => {
+  const axiosSecure = useAxiosSecure();
+  const { data: users = [] } = useQuery({
+    queryKey: ["users"],
+    queryFn: async () => {
+      const res = await axiosSecure.get("/users");
+      return res.data;
+    },
+  });
   return (
     <section className="">
       <SectionTitle subTile={"How Many ?"} heading={"Manage All Users"} />
       <div className="flex justify-between items-center uppercase bg-white p-2 mb-2 ">
-        <h2 className="text-xl text-black">Items:</h2>
-        <h2 className="text-xl text-black">Total Price:</h2>
-        <button className="btn btn-warning  text-black ">Pay</button>
+        <h2 className="text-xl text-black">Total User:{users.length}</h2>
       </div>
       <div className="overflow-x-auto ">
         <table className="table">
@@ -16,34 +24,32 @@ const AllUsers = () => {
           <thead className="bg-gray-500 text-white uppercase ">
             <tr>
               <th>SL</th>
-              <th>Item Image</th>
-              <th>Item Name</th>
-              <th>Price</th>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Role</th>
               <th>Action</th>
             </tr>
           </thead>
           <tbody>
             {/* row 1 */}
 
-            <tr className="text-gray-700">
-              <th>1</th>
-              <td>
-                <div className="flex items-center gap-3">
-                  <div className="avatar">
-                    <div className="mask mask-squircle h-12 w-12">
-                      <img src="" alt="Avatar Tailwind CSS Component" />
-                    </div>
-                  </div>
-                </div>
-              </td>
-              <td>name</td>
-              <td>$122</td>
-              <th>
-                <button className="btn btn-ghost btn-lg">
-                  <FaRegTrashAlt className="text-red-600" />
-                </button>
-              </th>
-            </tr>
+            {users.map((user, index) => (
+              <tr key={user._id} className="text-gray-700">
+                <th>{index + 1}</th>
+                <td>{user.name}</td>
+                <td>{user.email}</td>
+                <td>
+                  <button className="btn btn-ghost bg-orange-400 hover:bg-orange-800">
+                    <FaUser className="text-white" />
+                  </button>
+                </td>
+                <th>
+                  <button className="btn btn-ghost btn-lg">
+                    <FaRegTrashAlt className="text-red-600" />
+                  </button>
+                </th>
+              </tr>
+            ))}
           </tbody>
         </table>
       </div>
